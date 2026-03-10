23cf76d5a7a9382fe503cde583e90d6e0bb6cb80
ABIN PREM.T <abinprem2590@gmail.com>
2026-03-10 15:56:17 +0530

    added hosting config

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m(%^6380_ju1_46-$d8utg8h#pi+mtb#&0mq+*5_3cha15s@&1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
